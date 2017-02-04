d7a456f2c1ff77bec3a24f8ab44deffe6bd5ab9b
Fangdun Cai <cfddream@gmail.com>
2017-02-04 22:41:56 +0800

    Bump v0.0.1

--- index.js
+++ index.js
@@ -1,13 +1,110 @@
 'use strict'
 
-const jwt = require('jsonwebtoken')
+module.exports = makeJWT
 
-class JWT {
+const JWT = require('jsonwebtoken')
 
-  static install () {
+const defaults = {
+  key: 'user',
+  secret: undefined,
+  tokenLookup: 'header:Authorization',
+  authScheme: 'Bearer',
+  verifyOptions: undefined,
+  passthrough: false
+}
+
+function makeJWT (options = {}) {
+  options = Object.assign({}, defaults, options)
+
+  const { key, secret, tokenLookup, authScheme, verifyOptions, passthrough } = options
+
+  if (!secret) {
+    throw new Error('Missing the secret key')
   }
 
-  constructor () {
+  const [via, name] = tokenLookup.split(':')
+
+  let extractor = jwtFromHeader(name, authScheme)
+
+  switch (via) {
+    case 'query':
+      extractor = jwtFromQuery(name)
+      break
+    case 'cookie':
+      extractor = jwtFromCookie(name)
+      break
+    // no default
   }
 
+  return jwt
+
+  function jwt (ctx, next) {
+    const { token, error } = extractor(ctx)
+
+    if (!passthrough && error) {
+      return ctx.res.send(401, error)
+    } else if (passthrough && !token) {
+      return next()
+    }
+
+    return verify(token, secret, verifyOptions)
+      .then(result => {
+        ctx.state[key] = result
+      })
+      .catch(err => {
+        if (!passthrough) {
+          ctx.res.send(401, err)
+        }
+      })
+      .then(next)
+  }
+}
+
+function jwtFromHeader (header, authScheme) {
+  return getToken
+
+  function getToken (ctx) {
+    const auth = ctx.req.get(header) || ''
+    const [scheme, token = ''] = auth.split(' ')
+
+    return {
+      token,
+      error: !(scheme === authScheme && 0 < token.length) && 'Missing or invalid jwt in the request header'
+    }
+  }
+}
+
+function jwtFromQuery (name) {
+  return getToken
+
+  function getToken (ctx) {
+    const token = ctx.req.query[name]
+
+    return {
+      token,
+      error: !token && 'Missing jwt in the query string'
+    }
+  }
+}
+
+function jwtFromCookie (name) {
+  return getToken
+
+  function getToken (ctx) {
+    const token = ctx.cookies.get(name)
+
+    return {
+      token,
+      error: !token && 'Missing jwt in the cookie'
+    }
+  }
+}
+
+function verify (token, secret, options) {
+  return new Promise((resolve, reject) => {
+    JWT.verify(token, secret, options, (err, decoded) => {
+      if (err) return reject(err)
+      resolve(decoded)
+    })
+  })
 }
---
+++ test/index.js
@@ -0,0 +1,108 @@
+import test from 'ava'
+import request from 'request-promise'
+import Engine from 'trek-engine'
+import JWT from 'jsonwebtoken'
+import jwt from '..'
+
+const listen = app => {
+  return new Promise((resolve, reject) => {
+    app.run(function (err) {
+      if (err) {
+        return reject(err)
+      }
+      const { port } = this.address()
+      resolve(`http://localhost:${port}`)
+    })
+  })
+}
+
+test('should get error when missing the secret key', t => {
+  const app = new Engine()
+  const options = {}
+
+  const error = t.throws(() => {
+    app.use(jwt(options))
+  })
+
+  t.is(error.message, 'Missing the secret key')
+})
+
+test('should get user from header', async t => {
+  const secret = 'trek engine'
+  const app = new Engine()
+  const options = { secret }
+
+  app.use(jwt(options))
+
+  app.use(async ctx => {
+    ctx.res.send(200, ctx.state.user)
+  })
+
+  const url = await listen(app)
+  const res = await request({
+    url,
+    json: true,
+    simple: false,
+    resolveWithFullResponse: true,
+    headers: {
+      Authorization: 'Bearer ' + JWT.sign({ foor: 'bar' }, secret)
+    }
+  })
+
+  t.is(res.body.foor, 'bar')
+  t.is(res.statusCode, 200)
+})
+
+test('should get user from query', async t => {
+  const secret = 'trek engine'
+  const app = new Engine()
+  const options = { secret, tokenLookup: 'query:auth' }
+
+  app.use(jwt(options))
+
+  app.use(async ctx => {
+    ctx.res.send(200, ctx.state.user)
+  })
+
+  const url = await listen(app)
+  const res = await request({
+    url,
+    json: true,
+    simple: false,
+    resolveWithFullResponse: true,
+    qs: {
+      auth: JWT.sign({ foor: 'bar' }, secret)
+    }
+  })
+
+  t.is(res.body.foor, 'bar')
+  t.is(res.statusCode, 200)
+})
+
+test('should get user from cookie', async t => {
+  const secret = 'trek engine'
+  const app = new Engine()
+  const options = { secret, tokenLookup: 'cookie:auth' }
+
+  app.use(jwt(options))
+
+  app.use(async ctx => {
+    ctx.res.send(200, ctx.state.user)
+  })
+
+  const url = await listen(app)
+  const jar = request.jar()
+  const cookie = request.cookie('auth=' + JWT.sign({ foor: 'bar' }, secret))
+  jar.setCookie(cookie, url)
+  const res = await request({
+    url,
+    jar,
+    json: true,
+    simple: false,
+    resolveWithFullResponse: true
+  })
+
+  t.is(res.body.foor, 'bar')
+  t.is(res.statusCode, 200)
+})
+
